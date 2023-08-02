65eded9ccf985b63b43a4724dbfba58e0d446d4e
Prashanth Erumalla <naveennagam6015@gmail.com>
2023-08-02 17:50:29 +0530

    nav done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <div className='d-flex justify-content-around'>
+          <div>
+            <h2 className='m-4'>Tarokl</h2>
+          </div>
+          <div>
+            <ul className='d-flex'>
+              <li className='m-4 list-group-item'>
+                <a className='text-decoration-none text-secondary nav-list' href="https://cdn.dribbble.com/userupload/8970278/file/original
+                -eac5637addcd4edd70de4ed46d029c16.jpg?resize=1024x2997&vertical=center">Feature</a>
+              </li>
+              <li className='m-4 list-group-item'>
+                <a className='text-decoration-none text-secondary nav-list' href="https://cdn.dribbble.com/userupload/8970278/file/original
+                -eac5637addcd4edd70de4ed46d029c16.jpg?resize=1024x2997&vertical=center">Price</a>
+              </li>
+              <li className='m-4 list-group-item'>
+                <a className='text-decoration-none text-secondary nav-list' href="https://cdn.dribbble.com/userupload/8970278/file/original
+                -eac5637addcd4edd70de4ed46d029c16.jpg?resize=1024x2997&vertical=center">About</a>
+              </li>
+              <li className='m-4 list-group-item'>
+                <a className='text-decoration-none text-secondary nav-list' href="https://cdn.dribbble.com/userupload/8970278/file/original
+                -eac5637addcd4edd70de4ed46d029c16.jpg?resize=1024x2997&vertical=center">Community</a>
+              </li>
+            </ul>
+          </div>
+          <div className='m-4'>
+            <a href='' className='text-white btn-purple  text-decoration-none  nav-list'>Sign in</a>
+          </div>
+        </div>
+      </nav>
+      <section>
+      <a href='' className='text-white btn-purple  text-decoration-none  nav-list'>Start First 30 Day Trial</a>
+      </section>
+    </>
   );
 }
 
